34df014286ea57f8e8bd071c6ae169ba9ed790ec
Nhan <nnhann99@gmail.com>
2020-10-30 11:39:29 +0700

    Them random tong

--- source.cpp
+++ source.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 const double PI = 3.14159;
 float cVVuong(int a);
 float dTVuong(int a);
@@ -58,6 +61,24 @@ float cVTron(int a) {
 float dTTron(int a) {
 	return PI * a*a;
 }
+/////////////////////////////////////////////////////
+void phepCong(int &kq) {
+	int a = rand() % 100 + 1;
+	int b = rand() % 100 + 1;
+	cout << "so a :" << a << endl;
+	cout << "So b :" << b << endl;
+	cout << "a" << a << "+" << "b" << b << "=" << a + b;
+}
+bool kiemTra(int x, int a, int b) {
+	for (int i = 0; i < 100; i++) {
+		int tong = a + b;
+		if (tong == x)
+			return true;
+		else
+			return false;
+	}
+	
+}
 
 int main()
 {
@@ -89,7 +110,20 @@ int a;
 	cout << "Nhap ban kinh ht: ";
 	cin >> d;
 	cout << "Chu vi ht: " << cVTron(d) << " Dien tich hv: " << dTTron(d);
-
+//////////////////////////////////////////////
+int a, b, x;
+	srand(time(NULL));
+	a = (rand() % 100) + 1;
+	b = (rand() % 100) + 1;
+	cout << a << "+" << b << "=";
+	cin >> x;
+	if (kiemTra(x, a, b) == true) {
+		cout << "dung";
+	}
+	else {
+		cout << "sai";
+	
+	}
 	system("pause");
 	return 0;
 }
